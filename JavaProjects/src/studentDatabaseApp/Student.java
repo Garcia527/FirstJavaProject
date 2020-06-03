a88b073a9161f502368343e4b5ab9f55114050cf
Oscar Garcia <ogarcia@uwm.edu>
2020-06-03 18:02:50 -0500

    First Commit

---
+++ JavaProjects/src/studentDatabaseApp/Student.java
@@ -0,0 +1,17 @@
+package studentDatabaseApp;
+
+public class Student {
+
+	// Constructor: prompt user to enter student's name and year
+	
+	// Generate ID
+	
+	// Enroll in courses
+	
+	// View balance 
+	
+	// Pay tuition
+	
+	// Show status
+	
+}
---
+++ JavaProjects/src/studentDatabaseApp/StudentDatabaseApp.java
@@ -0,0 +1,12 @@
+package studentDatabaseApp;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+
+		// Ask how many users we want to add (n)
+		
+		// Create n number of students
+	}
+
+}
