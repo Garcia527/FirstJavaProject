16dbaf1eacb0b06ede88b67b96d96963f85689b7
Oscar Garcia <ogarcia@uwm.edu>
2020-06-04 10:08:53 -0500

    Added simple constructor and setStudentID method

--- JavaProjects/src/studentDatabaseApp/Student.java
+++ JavaProjects/src/studentDatabaseApp/Student.java
@@ -1,10 +1,43 @@
 package studentDatabaseApp;
 
+import java.util.Scanner;
+
 public class Student {
+	
+	private String firstName;
+	private String lastName;
+	private int gradeYear;
+	private String studentID;
+	private String courses;
+	private int tuitionBalance;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
 
 	// Constructor: prompt user to enter student's name and year
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter student's first name: ");
+		this.firstName = in.nextLine();
+		
+		System.out.print("Enter student's last name: ");
+		this.lastName = in.nextLine();
+		
+		System.out.print("1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student's class level: ");
+		this.gradeYear = in.nextInt();
+		
+		setStudentID();
+		
+		System.out.println(firstName +" "+ lastName +" "+ gradeYear +" "+studentID);
+		
+		
+	}
 	
 	// Generate ID
+	private void setStudentID() {
+		// Grade level + ID
+		id++;
+		this.studentID = gradeYear + "" + id;
+	}
 	
 	// Enroll in courses
 	
--- JavaProjects/src/studentDatabaseApp/StudentDatabaseApp.java
+++ JavaProjects/src/studentDatabaseApp/StudentDatabaseApp.java
@@ -3,7 +3,7 @@ package studentDatabaseApp;
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-
+		Student stu1 = new Student();
 		// Ask how many users we want to add (n)
 		
 		// Create n number of students
