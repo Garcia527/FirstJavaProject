4f9e7a481bd64ff7bef80af314707a259398514b
Oscar Garcia <ogarcia@uwm.edu>
2020-06-05 11:13:52 -0500

    Added payTuition and showInfo. Removed unnecessary prints.

--- JavaProjects/src/studentDatabaseApp/Student.java
+++ JavaProjects/src/studentDatabaseApp/Student.java
@@ -8,15 +8,15 @@ public class Student {
 	private String lastName;
 	private int gradeYear;
 	private String studentID;
-	private String courses;
-	private int tuitionBalance;
+	private String courses = "";
+	private int tuitionBalance = 0;
 	private static int costOfCourse = 600;
 	private static int id = 1000;
 
 	// Constructor: prompt user to enter student's name and year
 	public Student() {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter student's first name: ");
+		System.out.print("Enter student's first name: "); //Remove white spaces? 
 		this.firstName = in.nextLine();
 		
 		System.out.print("Enter student's last name: ");
@@ -25,10 +25,7 @@ public class Student {
 		System.out.print("1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student's class level: ");
 		this.gradeYear = in.nextInt();
 		
-		setStudentID();
-		
-		System.out.println(firstName +" "+ lastName +" "+ gradeYear +" "+studentID);
-		
+		setStudentID();		
 		
 	}
 	
@@ -40,11 +37,44 @@ public class Student {
 	}
 	
 	// Enroll in courses
+	public void enroll() {
+		// Get inside a loop, user hits 0 when they're done enrolling 
+		do {
+		System.out.print("Enter course to enroll (Q to quit): ");
+		Scanner in = new Scanner(System.in);
+		String course = in.nextLine();
+		if (!course.equals("Q")) {
+			courses = courses + "\n" + course;
+			tuitionBalance = tuitionBalance + costOfCourse;
+		}
+		else { break; } 
+		} while (1 != 0);	
+	}
 	
 	// View balance 
+	public void viewBalance() {
+		System.out.println("Your balance is: $" + tuitionBalance);
+	}
 	
 	// Pay tuition
+	public void payTuition() {
+		System.out.println();
+		viewBalance();
+		System.out.print("Please enter the amount you would like to pay: $");
+		Scanner in = new Scanner(System.in);
+		int payment = in.nextInt();
+		tuitionBalance = tuitionBalance - payment;
+		System.out.println("Thank you for your payment of $"+payment);
+		viewBalance();
+	}
 	
 	// Show status
+	public String showInfo() {
+		return "\n\nName: " + firstName + " " + lastName + 
+				"\nStudent's ID: " + studentID +
+				"\nGrade Year: " + gradeYear +
+				"\nCourses enrolled: " + courses + 
+				"\nBalance: $" + tuitionBalance;
+	}
 	
 }
--- JavaProjects/src/studentDatabaseApp/StudentDatabaseApp.java
+++ JavaProjects/src/studentDatabaseApp/StudentDatabaseApp.java
@@ -4,6 +4,9 @@ public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 		Student stu1 = new Student();
+		stu1.enroll();
+		stu1.payTuition();
+		System.out.println(stu1.showInfo());
 		// Ask how many users we want to add (n)
 		
 		// Create n number of students
